5640b3b8f9dcaebc7b404e55ae82c1913dde64d2
Andres <andres.nazarian@hotmail.com>
2021-09-24 15:30:12 -0300

    Updates

---
+++ precache-manifest.183d17ee87fb8e762456b504ab1d2275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966b738edc7043668186d332c7db64f6",
+    "url": "/ERC20_token_sale/index.html"
+  },
+  {
+    "revision": "7b5ac8c0961dec75afb8",
+    "url": "/ERC20_token_sale/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd48b81c6aea298b77c7",
+    "url": "/ERC20_token_sale/static/js/2.82d73542.chunk.js"
+  },
+  {
+    "revision": "7b5ac8c0961dec75afb8",
+    "url": "/ERC20_token_sale/static/js/main.6c447682.chunk.js"
+  },
+  {
+    "revision": "06f0e3497fb5272d41b7",
+    "url": "/ERC20_token_sale/static/js/runtime-main.523ad478.js"
+  }
+]);
